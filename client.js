6e883200eab24ed3a81f93ad895d4edf5af662c5
Logan Woolf <loganwoolf@gmail.com>
2022-01-19 23:13:05 +0000

    Move connect logic to client.js module

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = (host, port) => {
+  const conn = net.createConnection({
+    host,
+    port
+  });
+  conn.setEncoding('utf8');
+
+  conn.on("connect", (incomingData) => {
+    console.log('Connected!');
+    incomingData && console.log(`Connect Message: ${incomingData}`);
+  });
+
+  conn.on("data", (incomingData) => {
+    // runs when connection is first established
+    console.log(`Server Message: ${incomingData}`);
+  });
+
+  return {conn, host, port};
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,29 +1,11 @@
-const net = require('net');
+//entry point file
 
-const host = process.argv[2];
-const port = +process.argv[3];
+const connect = require('./client');
+const host = process.argv[2] || 'localhost';
+const port = +process.argv[3] || 8080;
 
-const connect = () => {
-  const conn = net.createConnection({
-    host,
-    port
-  });
-  conn.setEncoding('utf8');
-
-  conn.on("connect", (incomingData) => {
-    console.log('Connected!');
-    incomingData && console.log(`Connect Message: ${incomingData}`);
-  });
-
-  conn.on("data", (incomingData) => {
-    // runs when connection is first established
-    console.log(`Server Message: ${incomingData}`);
-  });
-
-  return conn;
-};
 console.log(`Connecting to ${host}:${port}`);
-connect();
+connect(host, port);
 
 //Information on Events for createConnection (an instance of class net.Socket)
 // https://nodejs.org/api/net.html#class-netsocket
