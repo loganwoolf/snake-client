ac4c6f1df9a4e11119789eb10f72c6aea3b71e91
Logan Woolf <loganwoolf@gmail.com>
2022-01-20 00:03:34 +0000

    Add event listener on std input, catch ctrl + c

--- play.js
+++ play.js
@@ -7,5 +7,22 @@ const port = +process.argv[3] || 8080;
 console.log(`Connecting to ${host}:${port}`);
 connect(host, port);
 
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+const setupInput = (() => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+})();
+
+
 //Information on Events for createConnection (an instance of class net.Socket)
 // https://nodejs.org/api/net.html#class-netsocket
